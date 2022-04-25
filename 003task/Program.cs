844af4c4323b2a99fc7cfd1036bbf58071f99161
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-25 16:58:35 +0300

    add 003 task

---
+++ 003task/Program.cs
@@ -0,0 +1,11 @@
+﻿/* 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N. */
+
+void Square(int N)
+{
+    int Sq = 1;
+    for (Sq = 1; Sq <= N; Sq++)
+        Console.WriteLine(Sq * Sq);
+}
+
+Square(5);
+Console.WriteLine();
