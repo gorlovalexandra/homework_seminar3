4164abe1dadc2f5f79b0460f270a35b9f14d7e8f
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-25 18:44:28 +0300

    add 005 task

---
+++ 005task/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 5. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. */
+
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double X = Math.Abs(x1 - x2);
+    double Y = Math.Abs(y1 - y2);
+    double Z = Math.Abs(z1 - z2);
+    double XYZ = Math.Sqrt((X * X) + (Y * Y) + (Z * Z));
+    return XYZ;
+}
+
+double AX = 3;
+double AY = 6;
+double AZ = 8;
+double BX = 2;
+double BY = 1;
+double BZ = -7;
+
+Console.WriteLine(Math.Round(Distance(AX, AY, AZ, BX, BY, BZ), 2));
