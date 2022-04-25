922cceabae6c1853e22eca3a7afeb81a066ef0bb
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-25 18:41:10 +0300

    add 004 task

---
+++ 004task/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 4. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. */
+
+void Palindrome(int N)
+{
+    if ((N % 10 == N / 10000) && ((N % 100) / 10 == (N / 1000) % 10))
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
+
+Palindrome(12821);
+Console.WriteLine();
