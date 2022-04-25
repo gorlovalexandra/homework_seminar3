85c7d35411b7b6628dd886e45cced24ab82618d3
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-25 16:51:09 +0300

    add 001 task

---
+++ 001task/Program.cs
@@ -0,0 +1,32 @@
+﻿/* 1. Напишите программу, которая принимает на вход координаты точки (X и Y),
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка. */
+
+void Quarter(double X, double Y)
+{
+    if (X == 0 && Y == 0)
+    {
+        Console.WriteLine("Enter X, Y > 0");
+    }
+
+    if (X > 0 && Y > 0)
+    {
+        Console.WriteLine("I quarter");
+    }
+
+    if (X < 0 && Y > 0)
+    {
+        Console.WriteLine("II quarter");
+    }
+
+    if (X < 0 && Y < 0)
+    {
+        Console.WriteLine("III quarter");
+    }
+    if (X > 0 && Y < 0)
+    {
+        Console.WriteLine("IV quarter");
+    }
+}
+
+Quarter(3, 6);
+Console.WriteLine();
