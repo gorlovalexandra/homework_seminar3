b5169b2ed8f29c5a7979bd1df0ec6311316b68dd
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-04-25 18:47:31 +0300

    add 006 task

---
+++ 006task/Program.cs
@@ -0,0 +1,11 @@
+﻿/* 6. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. */
+
+void Square(int N)
+{
+    int Sq = 1;
+    for (Sq = 1; Sq <= N; Sq++)
+        Console.WriteLine(Sq * Sq * Sq);
+}
+
+Square(3);
+Console.WriteLine();
